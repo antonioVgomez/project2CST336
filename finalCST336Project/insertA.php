de22b3c629406562fe85a2576ba5bf71937d7334
antonioVgomez <avillagomez@csumb.edu>
2017-12-13 22:15:41 -0800

    Update insertA.php

--- finalCST336Project/insertA.php
+++ finalCST336Project/insertA.php
@@ -79,7 +79,7 @@ if(isset($_GET['updateAthlete'])) {
             </form>
             <br>
             <form action="adminOnly.php">
-            <input type="submit" value="back" name="updateUser">
+            <input type="submit" value="back" >
         </form>
         <br>
         
